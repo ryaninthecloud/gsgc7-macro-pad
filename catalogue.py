8e6e84e0ddbbeedcd2f54242cd22f31abb1f99c1
Ryan Maloney <maloney.r@proton.me>
2026-05-08 13:40:45 +0100

    feat: hashing out a pattern for functions, defined lambda pattern in docstirng for catalogue

--- catalogue.py
+++ catalogue.py
@@ -9,7 +9,14 @@ strings:
     if begin with ! then printed to console
     if begin with # then are commands sent to pc
     
+Patterns for defining functions to be called
+--------------------------------------------
+After some thinking, it might be better to do
+lambda definitions for things like prints so that
+parsing a command can be done somewhat more easily.
 
+x = {'y': lambda a,b: print (a+b)} <-- pattern for prints
+Called in interface by x['y']('1','2') --> returns '12'
 '''
 
 def my_command():
@@ -19,7 +26,7 @@ windows_dispatch_catalogue = {
     'D' : {
         '1' : '!Someone is calling us!'
     },
-    'H' : my_command
+    'H' : lambda a,b: print(a+b)
 }
 
 macos_dispatch_catalogue = {
